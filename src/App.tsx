e9f97ed19191279cf568659004940c4e2c865282
CRomero19 <ronerodocarmojunior@hotmail.com>
2023-08-10 08:50:32 -0300

    feat:product-card-details

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ function App() {
   const teste = () => {
     console.log('testando123')
   }
+
+  const product = 
+  {name:'Miata ',description:'Lorem ipsum sla o que amet is just a language that i dont rlly know where it comes from', 
+  seller:'Mazda', km:'25.000',year:'1994',price:'145.000,00', higherThanFipe:false, active:true}
   
   return (
     <>
@@ -19,8 +23,9 @@ function App() {
         <Header/>
 
         <DefaultButton color={'--color-brand1'} text={'Button de teste'} textcolor={'--color-grey10'} bordercolor={'--color-random1'} type={'button'} buttonFunction={teste}/>
-        
-        <ProductCard/>
+
+        <ProductCard product={product}
+        />
 
         <RoutesMain/>
         
