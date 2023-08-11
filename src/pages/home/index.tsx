180a4b1cdfc8a1f43892f7f546e07b535219fe11
Gabriel Maciel <gabrielc.maciel42@gmail.com>
2023-08-11 09:35:54 -0300

    someone deleted that part of the code

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,16 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { FilterContext } from "../../context/FilterContext";
 import { ProductCard } from "../../components/ProductCard";
+import AnnouncementForm from "../../components/Forms/AnnouncementForm";
+import useAnnouncements from "../../hooks/useAnnouncements";
 
 export const Home = () => {
+    const { createAnnouncement } = useAnnouncements();
+    const [openAnnouncementModal, setOpenAnnouncementModal] = useState(false);
 
    const { Announcements, getAnnouncements } = useContext(FilterContext);
    
-   const dataTeste = {
+      const dataTeste = {
       brand: "",
       model: "",
       year: "",
@@ -29,6 +33,13 @@ export const Home = () => {
                <ProductCard key={announcement.id} announcement={announcement} />
             ))}
          </ul>
+         <AnnouncementForm
+        isCreateForm={true}
+        open={openAnnouncementModal}
+        setOpen={setOpenAnnouncementModal}
+        submitFunction={createAnnouncement}
+      />
       </main>
 )
 }
+
