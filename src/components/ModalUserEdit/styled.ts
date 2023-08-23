7d3ca28922d8c0f6edc04ab9fb30aac4047bb514
Rihan soares <rihan145@gmail.com>
2023-08-23 19:07:32 -0300

    feat: add styled modalEdit and modalDelete

---
+++ src/components/ModalUserEdit/styled.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const TitleContainer = styled.div`
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+   padding: 0rem 1rem;
+   img{
+      cursor: pointer;
+   }
+
+`
+export const StyledForm = styled.form`
+   display: flex;
+   flex-direction: column;
+   gap: 2rem;
+`
